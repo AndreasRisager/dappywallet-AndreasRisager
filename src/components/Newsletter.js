a737724f8ba9ce9e45f6bf803d362a8a71058d81
Andreas Desktop <andreas@barkov.dk>
2021-03-25 11:59:48 +0100

    s

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import NetlifyForm from 'react-ssg-netlify-forms';
 
-function formValidater(formValues) {
-	var atposition=formValues.indexOf("@");
-	var dotposition=formValues.lastIndexOf(".");
-	if (atposition<1 || dotposition<atposition+2 || dotposition+2>=formValues.length){
+function formValidater(value) {
+  var x = value.email;
+	var atposition=x.indexOf("@");
+	var dotposition=x.lastIndexOf(".");
+  var input = document.querySelector("input[type='email']");
+	if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+    input.style.border="1px solid red";
 		return false;
   }
   console.log("valid email");
+  input.style.border="1px solid green";
 	return true;
 }
 
 export default function Newsletter() {
-  const handleChange = e => setFormValues(e.target.value);
-	const [formValues, setFormValues] = useState("");
+  const handleChange = e => setFormValues({email: e.target.value});
+	const [formValues, setFormValues] = useState({email: ""});
   return (
-    <NetlifyForm formName="Newsletter" formValues={formValues} preSubmit={formValidater(formValues)} >
+    <NetlifyForm formName="Newsletter" formValues={formValues} preSubmit={() => formValidater(formValues)}>
       <input type="hidden" name="form-name" value="Newsletter" />
       <input type="email" name="email" value={formValues.email} onChange={handleChange} required placeholder="Your e-mail" />
       <button type="submit">Subscribe</button>
