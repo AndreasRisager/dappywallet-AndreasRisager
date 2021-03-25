d98fb2307e65543dfbbe029bc627a3abcb291ddb
Andreas Desktop <andreas@barkov.dk>
2021-03-25 13:26:36 +0100

    form validation

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,33 +1,27 @@
-import { useState } from "react";
-import NetlifyForm from 'react-ssg-netlify-forms';
-
-function formValidater() {
-  var input = document.querySelector("input[type='email']");
-  var x = input.value;
-	var atposition=x.indexOf("@");
-	var dotposition=x.lastIndexOf(".");
-	if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
-    input.style.border="1px solid red";
-    input.style.outlineColor = "red";
-    input.focus();
-		return false;
-  }
-  console.log("valid email");
-  input.style.border="1px solid green";
-  input.style.outlineColor = "green";
-	return true;
-}
+import { useState, useEffect } from "react";
 
 export default function Newsletter() {
-  const handleChange = e => setFormValues({ ...formValues, [e.target.name]: e.target.value });
-	const [formValues, setFormValues] = useState({email: ""});
+	const [emailValue, setEmailValue] = useState("");
 
-  console.log(formValues);
-  
+  useEffect(function() {
+    var atposition=emailValue.indexOf("@");
+    var dotposition=emailValue.lastIndexOf(".");
+    var email = document.querySelector("#newsletterEmail");
+    if(emailValue === "") {
+      email.style.border="1px solid #82899b";
+      return;
+    }
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=emailValue.length){
+      email.style.border="1px solid red";
+    } else {
+      email.style.border="1px solid green";
+    }
+  }, [emailValue])
+
   return (
-    <NetlifyForm formName="Newsletter" formValues={formValues} preSubmit={formValidater}>
-      <input type="email" name="email" value={formValues.email} onChange={handleChange} placeholder="Your e-mail" required />
+    <form noValidate onSubmit={(e) => e.preventDefault()}>
+      <input type="email" name="email" id="newsletterEmail" value={emailValue} onChange={(e) => setEmailValue(e.target.value)} placeholder="Your e-mail" required />
       <button type="submit">Subscribe</button>
-    </NetlifyForm>
+    </form>
   )
 }
